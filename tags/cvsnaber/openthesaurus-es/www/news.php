5433954b20946bf759f010ae9015f77c6651cfba
(no author) <(no author)@56f0d879-483a-0410-9027-ff7be8ecfbdc>
2007-09-15 20:04:20 +0000

    This commit was manufactured by cvs2svn to create tag 'cvsnaber'.

---
+++ tags/cvsnaber/openthesaurus-es/www/news.php
@@ -0,0 +1,13 @@
+<table border="0" cellpadding="2" cellspacing="0">
+<tr>
+	<td valign="top" align="right" width="90"><span class="newsdate">2007-07-12</span></td>
+	<td valign="top">Ab jetzt sind auf der Ergebnis-Seite auch Links
+		aus der Wikipedia integriert. Dabei handelt es sich nicht einfach um Synonyme,
+		sondern um Wörter, die im Wikipedia-Artikel zum aktuellen Suchbegriff verlinkt
+		sind. Damit eignet sich OpenThesaurus jetzt noch besser zum Auffinden von
+		Assoziationen. Beispiele zum Ausprobieren:
+		<a href="overview.php?word=Demokratie">Demokratie</a>,
+		<a href="overview.php?word=Welt">Welt</a>, <a href="overview.php?word=Wald">Wald</a>
+	</td>
+</tr>
+</table>
