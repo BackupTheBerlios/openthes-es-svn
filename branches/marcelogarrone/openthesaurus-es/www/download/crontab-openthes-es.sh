72bbd38413c6c29b7ce9604cd0d41a500e4f5dee
(no author) <(no author)@56f0d879-483a-0410-9027-ff7be8ecfbdc>
2007-09-15 22:52:01 +0000

    This commit was manufactured by cvs2svn to create branch 'marcelogarrone'.

---
+++ branches/marcelogarrone/openthesaurus-es/www/download/crontab-openthes-es.sh
@@ -0,0 +1,25 @@
+### text
+w3m -dump http://openthes-es.berlios.de/admin/text_export.php > ~/text_export.log
+sleep 5
+cd /home/groups/openthes-es/htdocs/download/
+tar -cjf Thesaurus_es_ES.txt.tar.bz2 COPYING Thesaurus_es_ES.txt
+
+### Kword
+w3m -dump http://openthes-es.berlios.de/admin/kword_export.php > ~/kword_export.log
+sleep 5
+tar -cjf Kword_thesaurus_es_ES.txt.tar.bz2 COPYING Kword_thesaurus_es_ES.txt
+
+### OOo
+w3m -dump http://openthes-es.berlios.de/admin/ooo_export.php > ~/ooo_export.log
+sleep 5
+cd ../OOo-Thesaurus
+tar -cjf OOo-thes_es_ES.tar.bz2 th_es_ES.* README_th_es_ES.txt COPYING 
+mv OOo-thes_es_ES.tar.bz2 ../download/
+
+### OOo2
+w3m -dump http://openthes-es.berlios.de/admin/ooo_new_export.php > ~/ooo_new_export.log
+sleep 5
+cd ../OOo2-Thesaurus
+tar -cjf OOo2-thes_es_ES.tar.bz2 th_es_ES_v2.idx th_es_ES_v2.dat README_th_es_ES_v2.txt COPYING
+mv OOo2-thes_es_ES.tar.bz2 ../download/
+
