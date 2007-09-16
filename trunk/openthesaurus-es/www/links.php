5d8084ec5d45392c5527f2c554035c4364161661
marcelogarrone <marcelogarrone@56f0d879-483a-0410-9027-ff7be8ecfbdc>
2007-09-16 15:04:17 +0000

    *** empty log message ***

--- trunk/openthesaurus-es/www/links.php
+++ trunk/openthesaurus-es/www/links.php
@@ -6,7 +6,10 @@
 		<!--<li><a href="background.php">Hintergrundinformationen</a></li>-->
 		<li><a href="https://lists.berlios.de/mailman/listinfo/openthes-es-general">Lista de Correo OpenThesaurus-es</a></li>
 		<li><a href="http://es.wikipedia.org">Wikipedia</a> / <a href="http://es.wiktionary.org">Wiktionary</a></li>
-		<li><a href="http://openthes-es.berlios.de">Proyecto OpenThesaurus-es</a></li>
+		<li><a href="http://www.wordreference.com/sinonimos/">Sin&oacute;nimos de la lengua espa&ntilde;ola</a></li>
+		<li><a href="http://www.wordreference.com/definicion/">Diccionario de la lengua espa&ntilde;ola</a></li>
+		<li><a href="http://developer.berlios.de/projects/openthes-es/">Proyecto OpenThesaurus-es</a></li>
+		
 		<!--<li>W&ouml;rterbuch Deutsch/Englisch:
 			<ul>
 			<li><a href="http://dict.tu-chemnitz.de/">dict.tu-chemnitz.de</a></li>
